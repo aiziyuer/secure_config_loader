2bd1b77cb2478f45e4d442f7802fe19a1db61a8c
aiziyuer <ziyu0123456789@gmail.com>
2025-03-23 20:47:11 +0800

    删除不必要的文件

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="secure-config-loader",
-    version="0.1.0",
+    use_scm_version=True, # 添加动态版本管理
+    setup_requires=["setuptools_scm"], # 添加依赖
     author="aiziyuer",
     author_email="910217951@qq.com",
     description="Secure multi-format configuration loader with Ansible Vault support",
